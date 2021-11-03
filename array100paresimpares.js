c6ae7a74cc75d47f3077d93a57d352b67f91afeb
Emiliano Franquet <emiliano@marketrealist.com>
2021-11-03 17:23:18 -0300

    ejercicios3

--- array100paresimpares.js
+++ array100paresimpares.js
@@ -1,40 +1,39 @@
-let num = 0;
-let count = 0;
-let suma = 1;
-let j = 2;
-let i;
-let primo = [];
-let lista = [];
-let pares = [];
-let impares = [];
-
-while ( num < 100 ){
-    num = num + suma;
-    count = count + 1;
-    lista.push(num);
-}
-for (i = 0; i < lista.length; i++) {
-    let numero = lista[i];
-    if (numero % 2 != 1) {
-       pares.push(numero);
-    } else {
-        impares.push(numero);
-    }
-    for (j = 2; j <= numero - 1; j++) {
-        let resto = numero % j;
-        if (resto == 0){
-            break;
-        }
-        if (j == numero - 1){
-            primo.push(numero);
-        }
-    }
-}
-
-console.log(lista);
-console.log(impares);
-console.log(pares);
-console.log(primo);
+// let num = 0;
+// let count = 0;
+// let j = 2;
+// let i;
+// let primo = [];
+// let lista = [];
+// let pares = [];
+// let impares = [];
+//
+// while ( num < 100 ){
+//     num++;
+//     count++;
+//     lista.push(num);
+// }
+// for (i = 0; i < lista.length; i++) {
+//     let numero = lista[i];
+//     if (numero % 2 != 1) {
+//        pares.push(numero);
+//     } else {
+//         impares.push(numero);
+//     }
+//     for (j = 2; j <= numero - 1; j++) {
+//         let resto = numero % j;
+//         if (resto == 0){
+//             break;
+//         }
+//         if (j == numero - 1){
+//             primo.push(numero);
+//         }
+//     }
+// }
+//
+// console.log(lista);
+// console.log(impares);
+// console.log(pares);
+// console.log(primo);
 // crear array de resultado
 // recorrer la lista
 // por cada numero N:
@@ -45,3 +44,33 @@ console.log(primo);
 //         si J == N -1 push al array de resultado
 //
 // imprimir array de resultado
+
+
+let lista = ['A', 'B', 'C', 'D'];
+let lista2 = [];
+let combinadorLetras = [];
+let s;
+
+    function combinador(lista) {
+        let resultados = [];
+        for (let s = 0; s < lista.length; s++) {
+            let letra = lista[s];
+            let letrasRestantes = lista.filter(function(item){return item != letra});
+            for (let lr = 0; lr < letrasRestantes.length; lr++){
+                let letraB = letrasRestantes[lr];
+                let str = letra + letraB;
+                let lRestanteB = letrasRestantes.filter(function(item){return item != letraB})
+                for (let lrb = 0; lrb < lRestanteB.length; lrb++){
+                    let letraC = lRestanteB[lrb];
+                    let lRestanteC = lRestanteB.filter(function(item){return item != letraC})
+                    let resultado = str + letraC + lRestanteC.pop();
+                    resultados.push(resultado);
+                }
+            }
+        }
+        return resultados;
+    }
+    console.log(combinador(lista));
+//
+// recorrer la lista
+// por cada letra LETRA RECORRIDA
