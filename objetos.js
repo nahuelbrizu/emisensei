d394e8df9d0daab1e1d66beb1e953270466f8a89
Emiliano Franquet <nahuelbrizu@outlook.com>
2021-11-05 17:04:37 -0300

    ejercicios331

---
+++ objetos.js
@@ -0,0 +1,72 @@
+let usuarios = [ {
+    nombre:"Nahuel",
+    apellido: "Brizuela",
+    edad: 29,
+    nacionalidad: "Argentina",
+    direccion: {
+        calle: "san carlos",
+        altura: 1392,
+        barrio: "Santos Lugares"
+    },
+    hijos: ["Cogo", "Charly"] },
+
+    {
+    nombre:"carlitos",
+    apellido: "estropito",
+    edad: 59,
+    nacionalidad: "Argentina",
+    direccion: {
+        calle: "sean carlos",
+        altura: 292,
+        barrio: "casonova"
+    },
+    hijos: ["samuel", "Carola", "ignacio"],
+
+}, {
+    nombre:"samuel",
+    apellido: "Cazuela",
+    edad: 89,
+    nacionalidad: "brazil",
+    direccion: {
+        calle: "jsasd",
+        altura: 1323,
+        barrio: "Hurlingam"
+    },
+    hijos: ["elsa", "salamin", "juan", "rogelio"],
+
+},
+    {
+    nombre:"cosme",
+    apellido: "fulanito",
+    edad: 29,
+    nacionalidad: "peru",
+    direccion: {
+        calle: "san zaraza",
+        altura: 322,
+        barrio: "ituzaingo"
+    },
+    hijos: ["jacinto", "juana", "soldana", "salamanca"]
+
+},
+    {
+        nombre: "roberta",
+        Apellido: "lamas",
+        edad: 40,
+        nacionalidad: "venezolana",
+        direccion: {
+            calle: "santa florencia",
+            altura: "123",
+            barrio: "jean black"
+        },
+        hijos: ["esteban"]
+    }
+    ]
+console.log(usuarios);
+
+// let lista = [4,425,63,445,34,53,45,3,4,3];
+// let result = lista.map(function(value){
+//     return value + 3;
+// })
+// for (let i = 0; lista.length; i++ ) {
+//     console.log(lista[i]);
+// }
