f2e8df16dab6b649a70316957982c53e331fc385
Emiliano Franquet <nahuelbrizu@outlook.com>
2021-11-08 16:17:25 -0300

    ejercicios331

--- objetos.js
+++ objetos.js
@@ -62,12 +62,55 @@ let usuarios = [ {
     }
     ];
 
-usuarios.forEach(function (usuario){
- console.log(usuario.nombre);
-})
+let contendor = [];
+// let mayorQ40 = usuarios.forEach(function (usuario){
+//     let numeros = usuario.nombre + " :" + usuario.edad;
+//     contendor.push(numb));
+//     console.log(numeros);
+// });
 
-//
-// console.log(usuarios);
+let mayorQ400 = usuarios.filter(function (usuario) {
+ return usuario.edad > 40;
+});
+console.log(mayorQ400)
+
+let rForEach = usuarios.forEach(function (usuario){
+    let edadUsuario = usuario.nombre + " : " + usuario.edad;
+    console.log(edadUsuario);
+    return edadUsuario;
+});
+console.log(rForEach);
+
+// console.log(resultadoForEach);
+let resultMap = usuarios.map(function (usuario){
+    console.log(usuario.nombre + " :" + usuario.edad);
+})
+let resultMap2 = usuarios.map(function (usuario){
+    let edadUsuario = usuario.nombre + " : " + usuario.edad;
+    console.log(edadUsuario);
+    return edadUsuario;
+});
+console.log(resultMap);
+console.log(resultMap2);
+
+let jsonPersonalizado = JSON.stringify(usuarios, null, '\t');
+console.log(jsonPersonalizado);
+
+let json = JSON.stringify(usuarios , ['nombre', 'edad', 'domicilio','direccion']);
+console.log(json);
+
+let jsonParse = JSON.parse(json);
+console.log(jsonParse);
+
+let jsonParse2 = JSON.parse(jsonPersonalizado);
+console.log(jsonParse2);
+
+
+
+
+
+
+// filter = funcion q devuelva un array de usuarios > 40
 
 // let lista = [4,425,63,445,34,53,45,3,4,3];
 // let result = lista.map(function(value){
